9d2121e65c395bce71431ad7c41504848e3721a6
Gaganal <gagani2912@gmail.com>
2024-07-02 10:58:14 +0530

    code editor

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CodeEditor from './components/codeeditor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{ textAlign: 'center' }}>Simple Code Editor</h1>
+        <CodeEditor />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/codeeditor.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-javascript';
+import * as esprima from 'esprima';
+
+const CodeEditor = () => {
+  const [code, setCode] = useState('');
+  const [codeChange, setCodeChange] = useState('');
+  const [error, setError] = useState('');
+
+  const handleChange = () => {
+    setCode(codeChange);
+    try {
+      esprima.parseScript(codeChange);
+      setError('');
+    } catch (err) {
+      console.error('Syntax Error:', err);
+      setError(err.description || err.message);
+    }
+  };
+
+  return (
+    <div className="code-editor">
+      <textarea
+        value={codeChange}
+        onChange={(e) => setCodeChange(e.target.value)}
+        spellCheck="false"
+        autoComplete="off"
+        autoCorrect="off"
+        style={{ borderColor: error ? 'red' : '#ddd' }}
+      />
+      <button
+        onClick={() => {
+          handleChange();
+        }}
+        style={{
+          padding: '9px 19px',
+          borderRadius: '6px',
+          backgroundColor: 'green',
+          color: 'white',
+          fontWeight: 600,
+        }}
+      >
+        RUN
+      </button>
+      {error && <div className="error-message">{error}</div>}
+      <pre>
+        <code
+          className="language-js"
+          dangerouslySetInnerHTML={{
+            __html: Prism.highlight(code, Prism.languages.javascript, 'javascript'),
+          }}
+        />
+      </pre>
+    </div>
+  );
+};
+
+export default CodeEditor;
